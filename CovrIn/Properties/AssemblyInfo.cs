d969def5c95a034cf78f05e26f1abfa40a77262c
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2015-01-30 01:10:28 +0100

    [#1] Added "Tests" as a friend assembly.
    To enable testing of internal classes.

--- CovrIn/Properties/AssemblyInfo.cs
+++ CovrIn/Properties/AssemblyInfo.cs
@@ -1,7 +1,10 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("CovrIn")]
 [assembly: AssemblyDescription("An open source tool to measure code coverage in .NET applications.")]
 [assembly: AssemblyCopyright("Copyright by Konrad M. Kruczyński 2015")]
 
 [assembly: AssemblyVersion("0.1")]
+
+[assembly: InternalsVisibleTo("Tests")]
