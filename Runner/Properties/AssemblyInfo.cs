9c8ef5a1a44663a9fd6a9493cb98020fad6aff7f
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2015-02-01 20:27:03 +0100

    [#2] Fixed AssemblyInfo

--- Runner/Properties/AssemblyInfo.cs
+++ Runner/Properties/AssemblyInfo.cs
@@ -1,27 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
-
 [assembly: AssemblyTitle ("CovrInRunner")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+[assembly: AssemblyDescription("An open source tool to measure code coverage in .NET applications.")]
+[assembly: AssemblyCopyright("Copyright by Konrad M. Kruczyński, Piotr Zierhoffer 2015")]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion ("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+[assembly: AssemblyVersion("0.1")]
