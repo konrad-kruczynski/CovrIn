d8a01158477674f4a193d215b960a365fd4d85a0
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2015-03-28 15:18:13 +0100

    [#10] Writer stub.

---
+++ Writer/Writer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Antmicro.Migrant;
+using System.IO;
+
+namespace CovrInWriter
+{
+    public static class Writer
+    {
+        static Writer()
+        {
+            var filename = "covrin.report.log"; //TODO: Add option
+            var fileMode = FileMode.Append; //TODO: Add option
+            fileStream = File.Open(filename, fileMode);
+            writer = new PrimitiveWriter(fileStream, false);
+        }
+
+        public static void Write(long blockId)
+        {
+            writer.Write(blockId);
+            //TODO: Add option to flush
+        }
+
+        private readonly static FileStream fileStream;
+        private readonly static PrimitiveWriter writer;
+    }
+}
+
