b5c76b9c36ca74dc95dacbe23f86384e9148a36d
Konrad M. Kruczyński <konrad.kruczynski@gmail.com>
2015-03-29 23:40:40 +0200

    [#14] More tests for namespace tokenizer.

--- Tests/Utilities/NamespaceTokenizerTests.cs
+++ Tests/Utilities/NamespaceTokenizerTests.cs
@@ -15,6 +15,20 @@ namespace CovrIn.Tests.Utilities
             CollectionAssert.AreEqual(new [] { N("Outer"), N("Inner") }, result);
         }
 
+        [Test]
+        public void ShouldTokenizeNestedClass()
+        {
+            var result = NamespaceTokenizer.Tokenize("Normal/Nested");
+            CollectionAssert.AreEqual(new [] { N("Normal"), I("Nested") }, result);
+        }
+
+        [Test]
+        public void ShouldTokenizeMixed()
+        {
+            var result = NamespaceTokenizer.Tokenize("A.B/C");
+            CollectionAssert.AreEqual(new [] { N("A"), N("B"), I("C") }, result);
+        }
+
         private NamespaceElement N(string name)
         {
             return new NamespaceElement(NamespaceElementType.Normal, name);
