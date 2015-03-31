e59f3074cf7573ea06c02ec348e504fa1592b053
Konrad M. Kruczyński <konrad.kruczynski@gmail.com>
2015-03-31 20:01:09 +0200

    [#25] Fixes #25.

--- Writer/Writer.cs
+++ Writer/Writer.cs
@@ -9,6 +9,7 @@ namespace CovrIn.Writer
     {
         static Writer()
         {
+            sync = new object();
             var filename = SimpleIniParser.ParseString(SettingsFileName, "traceFileName", DefaultOutputName);
             var fileMode = SimpleIniParser.ParseBool(SettingsFileName, "appendResults", true) ? FileMode.Append : FileMode.Create;
             fileStream = File.Open(filename, fileMode);
@@ -18,16 +19,20 @@ namespace CovrIn.Writer
 
         public static void Write(long blockId)
         {
-            writer.Write(blockId);
-            if(alwaysFlush)
+            lock(sync)
             {
-                fileStream.Flush();
+                writer.Write(blockId);
+                if(alwaysFlush)
+                {
+                    fileStream.Flush();
+                }
             }
         }
 
         private readonly static FileStream fileStream;
         private readonly static PrimitiveWriter writer;
         private readonly static bool alwaysFlush;
+        private readonly static object sync;
 
         private const string SettingsFileName = "covrin.settings";
         private const string DefaultOutputName = "covrin.report.log";
